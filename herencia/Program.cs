5768e487231411260bb8fb598954ac995c563100
Fernanda Millan <fernanda.millan@cetys.mx>
2026-04-21 09:18:53 -0700

    first commit

---
+++ herencia/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class ComidaMexicana
+{
+    public void PrepararBase()
+    {
+        Console.WriteLine("Tortilla + salsa + ingredientes base");
+    }
+}
+
+class Taco : ComidaMexicana
+{
+    public void Servir()
+    {
+        Console.WriteLine("Sirviendo taco");
+    }
+}
+
+class Sope : ComidaMexicana
+{
+    public void Servir()
+    {
+        Console.WriteLine("Sirviendo sope");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Taco taco = new Taco();
+        taco.PrepararBase();
+        taco.Servir();
+
+        Console.WriteLine();
+
+        Sope sope = new Sope();
+        sope.PrepararBase();
+        sope.Servir();
+    }
+}
